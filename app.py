43f962706682a162d82a8291a84d55b8e37d42cd
Rahul <rahul.ctg62@gmail.com>
2025-12-07 21:22:43 +0100

    added keras model

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from fastapi.middleware.cors import CORSMiddleware
 trained_model = tf.keras.models.load_model("ev_energy_model.keras")
 
 
-app = FastAPI()
+app = FastAPI("title="EV Energy Consumption API"")
 
 app.add_middleware(
     CORSMiddleware,
